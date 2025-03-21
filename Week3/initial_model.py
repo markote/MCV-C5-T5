08e4156592b4fe4267a251a00154520e556f02a8
marco <marco.terral@hotmail.com>
2025-03-21 13:51:19 +0100

    model init

--- Week3/initial_model.py
+++ Week3/initial_model.py
@@ -22,7 +22,6 @@ class Data(Dataset):
     def __init__(self, prefix,  partition, data_aug=False):
         self.prefix = prefix
         self.partition = partition
-        self.num_captions = 5
         self.max_len = TEXT_MAX_LEN
         if data_aug:
             self.img_proc = torch.nn.Sequential(
@@ -47,8 +46,7 @@ class Data(Dataset):
         img = self.img_proc(img)
     
         ## caption processing
-        caption = item.caption.reset_index(drop=True)[random.choice(list(range(self.num_captions)))]
-        cap_list = list(caption)
+        cap_list = list(title)
         final_list = [CHARS[0]]
         final_list.extend(cap_list)
         final_list.extend([CHARS[1]])
@@ -94,10 +92,10 @@ def optimizer_chooser(model, type_opt):
         print("Wrong model")
         sys.exit(1)
 
-def train(epochs, data, partitions, config=None):
-    data_train = Data(data, partitions['train'])
-    data_valid = Data(data, partitions['eval'])
-    data_test = Data(data, partitions['test'])
+def train(epochs, prefix, partitions, config=None):
+    data_train = Data(prefix, partitions['train'])
+    data_valid = Data(prefix, partitions['eval'])
+    data_test = Data(prefix, partitions['test'])
     dataloader_train = DataLoader(data_train, batch_size=config["batch_size"], pin_memory=True, shuffle=True, num_workers=8)
     dataloader_valid = DataLoader(data_test, batch_size=config["batch_size"], pin_memory=True, shuffle=False, num_workers=8)
     dataloader_test = DataLoader(data_test, batch_size=config["batch_size"], pin_memory=True, shuffle=False, num_workers=8)
@@ -106,13 +104,19 @@ def train(epochs, data, partitions, config=None):
     optimizer = optimizer_chooser(model, config["optimizer_type"])
     crit = nn.CrossEntropyLoss()
     metric = Metric()
+
+    bleu = evaluate.load('bleu')
+    meteor = evaluate.load('meteor')
+    rouge = evaluate.load('rouge')
+    metric = (bleue, rouge, meteor)
+
     for epoch in range(epochs):
         loss, res = train_one_epoch(model, optimizer, crit, metric, dataloader_train)
-        print(f'train loss: {loss:.2f}, metric: {res:.2f}, epoch: {epoch}')
+        print(f'train loss: {loss:.2f}, metric: {res}, epoch: {epoch}')
         loss_v, res_v = eval_epoch(model, crit, metric, dataloader_valid)
-        print(f'valid loss: {loss:.2f}, metric: {res:.2f}')
+        print(f'valid loss: {loss:.2f}, metric: {res}')
     loss_t, res_t = eval_epoch(model, crit, metric, dataloader_test)
-    print(f'test loss: {loss:.2f}, metric: {res:.2f}')
+    print(f'test loss: {loss:.2f}, metric: {res}')
     
 def train_one_epoch(model, optimizer, crit, metric, dataloader):
     model.train()    
@@ -120,6 +124,8 @@ def train_one_epoch(model, optimizer, crit, metric, dataloader):
     train_loss = 0.0
     correct = 0.0
     total = 0
+    b1,b2,rl,mt = 0.0, 0.0, 0.0, 0.0
+    total_batches = 0
     for images, titles in dataloader:
         images, titles = images.to(device), titles.to(device) # titles should be a tensor of shape (batch, num_seq vector) with each element being between [0, NUM_CHAR-1]
         
@@ -134,19 +140,44 @@ def train_one_epoch(model, optimizer, crit, metric, dataloader):
         optimizer.zero_grad()
         loss.backward()
         optimizer.step()
-
-        # Track loss and accuracy
+        
+        # Track loss and metrics
         b,_,seq_size = outputs.shape
-        train_loss += loss.item() * b * seq_size # total loss avg for each char
-        _, predicted = outputs.max(1) # we are interested on the pos of the max logits for the NUM_CHAR dimension so basically the predicted char
-        correct += (predicted == titles).sum().item() # avg accuracy for each char
-        total += titles.shape[0]*titles.shape[1]
+
+        _, predicted = outputs.max(1) # we are interested on the pos of the max logits for the NUM_CHAR dimension so basically the predicted char (batch, num_seq vector) with each element being between [0, NUM_CHAR-1] 
+        idx_titles_T = titles.T  # Shape: (seq_len, batch_size)
+        idx_predicted_T = predicted.T 
+        gt = [[IDX2CHAR[idx.item()] for idx in seq] for seq in idx_titles_T]
+        pred = [[IDX2CHAR[idx.item()] for idx in seq] for seq in idx_predicted_T]
+
+        # concat gt and pred and do the val after the whole training? Instead of computing by batch
+        
+        print(f"GT: {gt}, Pred: {pred}")
+        bleue, rouge, meteor = metric
+        bleu1 = bleu.compute(predictions=pred, references=gt, max_order=1)
+        bleu2 = bleu.compute(predictions=pred, references=gt, max_order=2)
+        res_r = rouge.compute(predictions=pred, references=gt)
+        res_m = meteor.compute(predictions=pred, references=gt)
+        
+        b1 += bleu1['bleu']
+        b2 += bleu2['bleu']
+        rl += res_r['rougeL']
+        mt += res_m['meteor']
+       
+        train_loss += loss.item() * b # compute the avg loss by sum of all seq chars
+        total += seq_size
+        total_batches += 1
 
     # Calculate training metrics
     avg_train_loss = train_loss / total
-    train_accuracy = correct / total
-    
-    return avg_train_loss, train_accuracy
+    b1 = b1 / total_batches
+    b2 = b2 / total_batches
+    rl = rl / total_batches
+    mt = mt / total_batches
+
+    result = f"BLEU-1:{b1*100:.1f}%, BLEU2:{b2*100:.1f}%, ROUGE-L:{rl*100:.1f}%, METEOR:{mt*100:.1f}%"
+
+    return avg_train_loss, result
 
 def eval_epoch(model, crit, metric, dataloader):
     model.eval()    
@@ -165,6 +196,7 @@ def eval_epoch(model, crit, metric, dataloader):
             loss = 0.0
             loss = crit(outputs, titles).sum() 
             # print(loss)
+            
 
             # Track loss and accuracy
             b,_,seq_size = outputs.shape
@@ -186,6 +218,15 @@ if __name__ == "__main__":
     img_path = f'{base_path}FoodImages/'
     splits_path = f'{base_path}DataSplit.npy'
 
+    config = {
+            "prefix": "/ghome/c5mcv05/image_captioning_dataset/FoodImages",
+            "testdata_path": "~/datanew/MIT_small_train_2/test",
+            "batch_size": 32,
+            "optimizer_type": "SGD",
+            "num_epochs": 1,
+        }
+
     partitions = np.load(splits_path, allow_pickle=True).item()
+    train(epoch["num_epochs"], config["prefix"], partitions, config)
 
 
